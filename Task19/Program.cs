1236f76a389556c0a254b5aed88a176e225d8ac1
Юрий Мухаметзянов <YusufuMukhametzyanov@yandex.ru>
2022-10-10 22:32:13 +0500

    Add result tasks 19, 21, 23

---
+++ Task19/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+/*var s = "232";
+if (s.Reverse().SequenceEqual(s)) Console.WriteLine("Палиндром!");*/
+
+/*int[] array = { 2, 2, 3, 2, 2 };
+
+int n = array.Length;
+
+
+if (array[0] == array[4] && array[1] == array[3])
+
+    Console.WriteLine("Число является палиндромом");
+else
+    Console.WriteLine("Число не является палиндромом");*/
+
+Console.Write("Введите пятизначное число: ");
+ string a = Console.ReadLine();
+
+
+if (a[0] == a[4] && a[1] == a[3])
+
+    Console.WriteLine($"{a} -> да");
+else
+    Console.WriteLine($"{a} -> нет");
+
+
+   
+
+
+
---
+++ Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Задача 23
+Напишите программу, которая на вход принимает число (N) и выдаёт таблицу кубов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25
+2 -> 1, 4
+*/
+
+void GetNumber (int number)
+{
+    Console.Write($"{number} -> ");
+    for (int i = 1; i <= number; i++)
+    {
+        double a = Math.Pow(i,3);
+        Console.Write($" {a} ");
+    }
+}
+
+Console.Write("Введите число:");
+int b = int.Parse(Console.ReadLine());
+GetNumber(b);
